7b531395c3b8b794f0c16ef7408d9175097e5230
Tran Trung Thanh <umbalaconmeogia@gmail.com>
2025-02-04 16:49:37 +0900

    Add zip into linux-backupDb.sh

--- bin/backupDbMySQL/linux-backupDb.sh
+++ bin/backupDbMySQL/linux-backupDb.sh
@@ -5,10 +5,19 @@ ROOT_PATH=../
 # Use yyyymmdd_HHiiss as container backup directory.
 CONTAINER_DIR=`date "+%Y%m%d_%H%M%S"`
 # Database name.
-DB_NAME=db_name
+DB_NAME=dtminternal_salesplandev
 # backup directory name.
 BACKUP_DIR=backup
 BACKUP_PATH=$ROOT_PATH/$BACKUP_DIR
+CONTAINER_PATH=$BACKUP_PATH/$CONTAINER_DIR
 
+# Create backup directory.
+mkdir $CONTAINER_PATH
 # Copy mysql
-mysqldump --defaults-file=$BACKUP_PATH/my.cnf $DB_NAME > ${DB_NAME}_${CONTAINER_DIR}.sql
+mysqldump --defaults-file=$BACKUP_PATH/my.cnf $DB_NAME > $CONTAINER_PATH/${DB_NAME}.sql
+# Copy app
+# cp -pR $ROOT_PATH/$SOURCE_DIR $CONTAINER_PATH/
+cd $BACKUP_PATH
+zip -r ${CONTAINER_DIR}.zip $CONTAINER_DIR
+# Delete backup directory
+rm -fr $CONTAINER_PATH
