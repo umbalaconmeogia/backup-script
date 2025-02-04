65b1f0772a87295a772d6a72a9310604f0175c96
Tran Trung Thanh <umbalaconmeogia@gmail.com>
2025-02-04 13:56:36 +0900

    Update linux-backupDb.sh

--- bin/backupDbMySQL/linux-backupDb.sh
+++ bin/backupDbMySQL/linux-backupDb.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
-# Root directory (parent of $BACKUP_DIR and $SOURCE_DIR)
-ROOT_PATH=/home/thanh/web/com.nashiari.dev/201405.roll_call
-# Default config
-PATH=/usr/local/sbin:/usr/bin:/bin
+# Root directory (parent of $BACKUP_DIR)
+ROOT_PATH=../
 # Use yyyymmdd_HHiiss as container backup directory.
 CONTAINER_DIR=`date "+%Y%m%d_%H%M%S"`
 # Database name.
-DB_NAME=roll_call
+DB_NAME=db_name
 # backup directory name.
 BACKUP_DIR=backup
-# Source directory name.
-SOURCE_DIR=roll_call
 BACKUP_PATH=$ROOT_PATH/$BACKUP_DIR
-CONTAINER_PATH=$BACKUP_PATH/$CONTAINER_DIR
 
 # Copy mysql
 mysqldump --defaults-file=$BACKUP_PATH/my.cnf $DB_NAME > ${DB_NAME}_${CONTAINER_DIR}.sql
